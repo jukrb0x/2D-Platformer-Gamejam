8d991962cbb1a0ef26de65f122c155b167c28af5
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2022-01-06 21:48:51 +0800

    feat: add spites

---
+++ Assets/Scripts/Controller/GameController.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private void Awake()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Update()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void FixedUpdate()
+    {
+        throw new NotImplementedException();
+    }
+}
