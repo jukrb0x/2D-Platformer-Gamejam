fbfdc9c4d979a4d047afc482f8e86d2a1fc88f93
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2022-01-07 14:43:27 +0800

    feat: add health bar

---
+++ Assets/Scripts/Environment/Key.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        // TODO pass level
+    }
+}
---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private void Start()
+    {
+        // TODO
+        throw new NotImplementedException();
+    }
+}
