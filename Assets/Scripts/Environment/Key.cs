f8872875c7066c3639bf6c17280d2db54934294f
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2022-01-07 17:45:16 +0800

    win test

--- Assets/Scripts/Environment/Key.cs
+++ Assets/Scripts/Environment/Key.cs
@@ -6,5 +6,9 @@ public class Key : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D col)
     {
         // TODO pass level
+        if (col.CompareTag("Player"))
+        {
+            print("you win");
+        }
     }
 }
